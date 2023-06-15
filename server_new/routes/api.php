23bcaf5d91e5ae445949ca177176409965d7da7a
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-15 10:18:59 +0900

    Revert "Add Users Events Groups Routes and Controllers"
    This reverts commit e68e8934b6c522aab301639d852645d7c0e5b260.

--- server_new/routes/api.php
+++ server_new/routes/api.php
@@ -23,3 +23,18 @@ Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+//Groups Routes
+Route::get('/groups', [GroupController::class, 'index']);
+Route::get('/groups/{id}', [GroupController::class, 'show']);
+Route::post('/groups', [GroupController::class, 'store']);
+Route::put('/groups/{id}', [GroupController::class, 'update']);
+Route::delete('/groups/{id}', [GroupController::class, 'destroy']);
+
+
+//Events Routes
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+Route::put('/events/{id}', [EventController::class, 'update']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
