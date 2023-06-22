ae9295603de2a6be2fee3c2dbc2e3a967c2217e7
Le Thi Yen Nhi <nhile0084@gmail.com>
2023-06-22 11:35:58 +0900

    gender

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -667,11 +667,12 @@ function Header() {
 
                 >
 
-                  <option value="0">Male</option>
+                  <option value="0">-- Select --</option>
+                  <option value="1">Male</option>
 
-                  <option value="1">Female</option>
+                  <option value="2">Female</option>
 
-                  <option value="2">Orther</option>
+                  <option value="3">Orther</option>
 
                 </select>
 
