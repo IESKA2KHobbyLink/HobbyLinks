42124d6cd86e84d32f41e777aea4917b2763b182
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-07-04 00:37:10 +0900

    implement frontend filter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,21 @@ import EventPage from "./pages/EventPage";
 import CreateEventPage from "./pages/CreateEventPage";
 import CreateGroupPage from "./pages/CreateGroupPage";
 import Header from "./components/Header";
+import { SearchProvider } from "./components/SearchContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/group/:groupId' element={<GroupPage />} />
-        <Route path='/event/:eventId' element={<EventPage />} />
-        <Route path='/createEvent' element={<CreateEventPage />} />
-        <Route path='/createGroup' element={<CreateGroupPage />} />
-      </Routes>
+      <SearchProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/group/:groupId' element={<GroupPage />} />
+          <Route path='/event/:eventId' element={<EventPage />} />
+          <Route path='/createEvent' element={<CreateEventPage />} />
+          <Route path='/createGroup' element={<CreateGroupPage />} />
+        </Routes>
+      </SearchProvider>
     </BrowserRouter>
   );
 }
