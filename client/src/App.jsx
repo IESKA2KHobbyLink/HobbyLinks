a830decbb6c17bfb724f355a63d6d172229696b4
Le Thi Yen Nhi <nhile0084@gmail.com>
2023-07-11 00:32:42 +0900

    add UserProfile のlink

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,20 @@ import EventPage from "./pages/EventPage";
 import CreateEventPage from "./pages/CreateEventPage";
 import CreateGroupPage from "./pages/CreateGroupPage";
 import Header from "./components/Header";
+import UserProfile from "./pages/UserProfile";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage />} />
         <Route path='/group/:groupId' element={<GroupPage />} />
         <Route path='/event/:eventId' element={<EventPage />} />
         <Route path='/createEvent' element={<CreateEventPage />} />
         <Route path='/createGroup' element={<CreateGroupPage />} />
+        <Route path='/userProfile' element={<UserProfile />} />
       </Routes>
     </BrowserRouter>
   );
