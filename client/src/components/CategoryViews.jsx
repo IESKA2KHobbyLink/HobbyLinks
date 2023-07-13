17859a63f41096bab093e36df1755dccad5b53de
huru-to2 <tomo88407smwjj@gmail.com>
2023-07-13 16:46:18 +0900

    少しデザイン

--- client/src/components/CategoryViews.jsx
+++ client/src/components/CategoryViews.jsx
@@ -5,6 +5,7 @@ function CategoryViews() {
         <div className='w-96 h-64 fixed'>
             <p className="text-5xl text-center">カテゴリー</p>
             <ul className="h-full text-xl overflow-y-scroll w-auto">
+                {/* カテゴリーをforeachみたいなので回す */}
                 <li className="rounded bg-white border-b-4" >赤ちゃん本舗</li>
 
                 <li></li>
